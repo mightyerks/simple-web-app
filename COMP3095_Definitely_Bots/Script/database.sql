fc69fc7a09763395a7814c1ffa6132651470e81f
mightyerks <36455835+mightyerks@users.noreply.github.com>
2019-03-17 19:45:44 -0400

    working

---
+++ COMP3095_Definitely_Bots/Script/database.sql
@@ -0,0 +1,27 @@
+DELETE DATABASE IF EXISTS COMP3095;
+
+CREATE DATABASE IF NOT EXISTS COMP3095;
+USE COMP3095;
+grant all on COMP3095.* to 'admin'@'localhost' identified by 'admin'; 
+
+CREATE TABLE USERS 
+( 
+	id int(11) AUTO_INCREMENT PRIMARY KEY, 
+	
+	firstname varchar(255) NOT NULL,
+	
+	lastname varchar(255) NOT NULL,
+	
+	address varchar(255) NOT NULL, 
+	
+	email varchar(255) NOT NULL, 
+	
+	role ENUM('a','b') NOT NULL DEFAULT 'a',
+	
+	created timestamp default current_timestamp,
+	
+	password varchar(20) NOT NULL
+);
+
+INSERT INTO `USERS` (`firstname`, `lastname`, `email`, `role`, `password`) VALUES
+(NULL, NULL, `admin@domain.ca`, NULL, `P@ssword1`);
